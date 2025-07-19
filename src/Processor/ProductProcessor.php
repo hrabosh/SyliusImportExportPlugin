3fa4a493a13ddfb4003f1d3f20d41bd20b3faf2f
Jan Hrabovsky <hrabovskyjan1@gmail.com>
2025-07-19 11:34:43 +0200

    utf8 sanitazition mintiga-eshop

--- src/Processor/ProductProcessor.php
+++ src/Processor/ProductProcessor.php
@@ -244,7 +244,7 @@ final class ProductProcessor implements ResourceProcessorInterface
             $short = $data['Short_description'] ?? '';
 
             if (!mb_check_encoding($short, 'UTF-8')) {
-                file_put_contents('/var/log/problematic.csv', $short . PHP_EOL, FILE_APPEND);
+                dump($short);
                 dump('Invalid encoding: ', $short);
             }
 
